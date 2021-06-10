ed98550250ebafb3a6dcf6a88e2ae2ca9f026c09
Asma Qureshi <asma.farhat2003@gmail.com>
2021-06-10 10:33:18 +0100

    added db config

--- config.js
+++ config.js
@@ -11,7 +11,12 @@ require("dotenv").config();
 
 //const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}; //heroku addons
 
 const config =  process.env.NODE_ENV === "production" ? proConfig : devConfig;
 
